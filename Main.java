3f20f999180143b916a9e4d93cf7bef8d23a23ef
genimilitairefabrice18-spec <genimilitairefabrice18@gmail.com>
2026-03-11 07:49:52 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Salut depuis VS Code !");
+        System.out.println("C'est Fabrice");
+        int x = 5;
+        int y = 7;
+        System.out.println("la summe est :" + x + y);
+        System.out.println("La somme est :" +(x + y ));
+
+        int k, l, z;
+        k=l=z=50;
+        System.out.println(k+l+z);
+
+        String studentName = "John Doe";
+        int studentID = 12;
+        int studentAge = 54;
+        float studentFee = 7550.63f ;
+        char studentGrade = 'B' ;
+
+        //les sorties
+
+        System.out.println("Student Name is: " + studentName);
+        System.out.println("Student Id is: " + studentID);
+        System.out.println("Student age is: " + studentAge);
+        System.out.println("Student fee is: " + studentFee);
+        System.out.println("Student grade is: " + studentGrade);
+
+    int items = 50;
+    float costPerItem = 9.99f;
+    float totalCost = items * costPerItem;
+    char currency = '$';
+
+    // Print variables
+    System.out.println("Nombre d’articles: " + items);
+    System.out.println("Coût par article: " + costPerItem + currency);
+    System.out.println("Coût par article = " + totalCost + currency);
+    }
+    }
+    
